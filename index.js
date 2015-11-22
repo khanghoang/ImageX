4615a4cbdd3776f2cbe43403ba67e87496177609
Khang Hoang <hoangtrieukhang@gmail.com>
2015-11-22 15:01:09 +0000

    config with width and height

--- index.js
+++ index.js
@@ -5,33 +5,6 @@ var http = require('http');
 var https = require('https');
 var app = express();
  
-var convert = gm({
-  pool: 5,             // how many graphicsmagick processes to use 
-  format: 'png',       // format to convert to 
-  scale: {
-    width: 200,        // scale input to this width 
-    height: 200,       // scale input this height 
-    type: 'contain'    // scale type (either contain/cover/fixed) 
-  },
-  crop: {
-    width: 200,        // crop input to this width 
-    height: 200,       // crop input this height 
-    x: 0,              // crop using this x offset 
-    y: 0               // crop using this y offset 
-  },
-  page: [1,5],         // only render page 1 to 5 (for pdfs) 
-                       // set to a single number if you only want to render one page 
-                       // or omit if you want all pages 
-  rotate: 'auto',      // auto rotate image based on exif data 
-                       // or use rotate:degrees 
-  density: 300,        // set the image density. useful when converting pdf to images 
-  split: false,        // when converting pdfs into images it is possible to split 
-                       // into multiple pages. If set to true the resulting file will 
-                       // be a tar containing all the images. 
-  tar: false           // stream a tar containing the image. This is forced to `true` 
-                       // if split is set to `true` 
-})
-
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -39,17 +12,24 @@ app.get('/', function (req, res) {
 app.get('/api/image', function(req, res) {
     var url = req.query.url;
     var request;
+    var resize = gm({
+  	pool: 20,             // how many graphicsmagick processes to use 
+  	format: 'png',       // format to convert to 
+	scale: {
+	width: req.query.w || 100,
+	height: req.query.h || 100,
+    	type: 'contain'    // scale type (either contain/cover/fixed) 
+	}})
+
     if(url) {
       if(!!~url.toLowerCase().indexOf("https://")) {
         request = https.get(url, function(response) {
-          response.pipe(convert())
-		.pipe(convert({scale:300, rotate:180, format:'png'}))
+          response.pipe(resize())
 		.pipe(res)
         });
       } else if (!!~url.toLowerCase().indexOf("http://")) {
         request = http.get(url, function(response) {
-          response.pipe(convert())
-		.pipe(convert({scale:300, rotate:180, format:'png'}))
+          response.pipe(resize())
 		.pipe(res)
         });
       } else {
